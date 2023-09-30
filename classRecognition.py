2a1bba15029f87189bd4ce4ad4678df32d0f3a09
Wasniewski <Mikolaj.Wasniewski@aptiv.com>
2023-09-30 21:35:02 +0200

    embeddings

--- classRecognition.py
+++ classRecognition.py
@@ -1,5 +1,7 @@
 from torchvision.io import read_image
 from torchvision.models import resnet50, ResNet50_Weights
+import torch.nn as nn
+
 
 img = read_image("./dog.jpg")
 
@@ -10,8 +12,10 @@ model.eval()
 preprocess = weights.transforms()
 
 batch = preprocess(img).unsqueeze(0)
+modelNoFN = nn.Sequential(*list(model.children())[:-1])
 
 prediction = model(batch).squeeze(0).softmax(0)
+embeddedRepresentation = modelNoFN(batch).squeeze(0)
 class_id = prediction.argmax().item()
 score = prediction[class_id].item()
 category_name = weights.meta["categories"][class_id]
